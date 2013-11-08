f1a07b0bbefa25e1ade506bf0e23fc7e12d3dac3
Erik Villegas <erikv@dobango.com>
2013-11-07 22:45:29 -0800

    mongo integration

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ app.use(express.logger());
 
 var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://erik:database3@ds053638.mongolab.com:53638/heroku_app19288256';
 
-mongo.Db.connect(mongoUri, function (err, db) {
+mongo.db.connect(mongoUri, function (err, db) {
   db.collection('mydocs', function(er, collection) {
     collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
     });
