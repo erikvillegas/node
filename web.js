791f067978856835f6d8f88cb86ddb029d8a0a7c
Erik Villegas <erikv@dobango.com>
2013-11-07 22:43:36 -0800

    mongo integration

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
-var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://ds053638.mongolab.com:53638/heroku_app19288256';
 
 mongo.Db.connect(mongoUri, function (err, db) {
   db.collection('mydocs', function(er, collection) {
