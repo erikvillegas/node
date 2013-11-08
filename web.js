1d7c544573d0503e57f0692b58c5b4218c1d67cc
Erik Villegas <erikv@dobango.com>
2013-11-07 22:36:26 -0800

    mongo integration

--- web.js
+++ web.js
@@ -1,12 +1,25 @@
 var express = require("express");
-var app = express();
-app.use(express.logger());
+var mongo = require('mongodb');
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+// var app = express();
+// app.use(express.logger());
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+// app.get('/', function(request, response) {
+//   response.send('Hello World!');
+// });
+
+// var port = process.env.PORT || 5000;
+// app.listen(port, function() {
+//   console.log("Listening on " + port);
+// });
+
+
+
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
 });
